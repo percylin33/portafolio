204c2ca8c3014f9af4d14d16c9cbe6fc2034a7cf
percy <percy.u.lomas@gmail.com>
2023-10-04 17:22:23 -0500

    responseve-project-1

--- src/app/Proyect/page.tsx
+++ src/app/Proyect/page.tsx
@@ -3,7 +3,7 @@ import ImageCarousel from "@/components/Carrousel";
 import Proyectos from "@/components/Proyectos";
 
 const Proyect = () => {
-    return <div className="bg-gray-100 h-[calc(100vh-4rem)] w-screen"> 
+    return <div className="bg-gray-100 h-full w-screen md:h-screen lg:h-[calc(100vh-4rem)] "> 
       <Proyectos/>
     
       </div>
