fc7cddbbef65a82d61cd90e9a7391b75b33d2352
percy <percy.u.lomas@gmail.com>
2023-10-04 16:39:30 -0500

    styles-response2

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 import about from "@/acces/about.jpeg"
 
 const About = () => {
-    return <div className="bg-gray-100  items-center h-full w-screen md:flex md:h-[calc(100vh-4rem)]"> 
-      <div className="w-full flex justify-center py-8         md:w-2/5 ">
+    return <div className="bg-gray-100  items-center h-full w-screen lg:flex lg:h-[calc(100vh-4rem)]"> 
+      <div className="w-full flex justify-center py-8 lg:w-2/5 ">
         <Image src={about} alt="foto about" className="w-4/5 h-auto rounded-lg  "/>
 
       </div>
-      <div className="w-full p-2 md:w-3/5 md:pr-4 ">
+      <div className="w-full p-2 lg:w-3/5 lg:pr-4 ">
         <p className="  pl-4  text-4xl font-bold ">Experiencia Academica </p>
         
         <p className="p-4 text-lg">
