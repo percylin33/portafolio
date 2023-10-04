c6c37be36485877e53af3ea107c45e186efb2226
percy <percy.u.lomas@gmail.com>
2023-10-04 13:49:02 -0500

    styles-resomposive1-home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,15 +26,15 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="bg-gray-100 flex items-center h-screen w-screen">
-      <div className="pb-10 w-1/2">
+    <div className="bg-gray-100  items-center h-full w-screen lg:h-[calc(100vh-4rem)] lg:flex ">
+      <div className="pb-10 w-full pt-4 lg:w-1/2">
         <div className="bg-fondo flex justify-center w-[60%] mx-auto text-center rounded-lg ">
-          <Image src={photos[currentPhotoIndex]} alt="my photo" className={`rounded-lg h-96 w-80 m-auto text-center transition-opacity ${fadeOut ? "opacity-0" : "opacity-100"}`} />
+          <Image src={photos[currentPhotoIndex]} alt="my photo" className={`rounded-lg h-64 sm:h-96 w-80 m-auto text-center transition-opacity ${fadeOut ? "opacity-0" : "opacity-100"}` } />
         </div>
       </div>
-      <div className="w-1/2">
-        <h1 className="text-4xl pb-10 pr-20 font-bold">Hola!! Soy <span className="text-cyan-800 ">Percy Valderrama</span>, Bienvenidos a mi Portafolio</h1>
-        <p className="text-xl pr-20">La creatividad es la chispa que da vida a mi trabajo👩‍💻. Cada proyecto es un lienzo en blanco donde puedo dar vida a mis ideas y expresar mi pasión por el codigo. <br /> <br /> Bienvenido a mi mundo creativo 🎨, donde cada detalle cuenta y cada pixel tiene un propósito. Explora mi portafolio y descubre cómo puedo ayudarte a dar vida a tus proyectos.</p>
+      <div className="w-full p-4 lg:w-1/2 lg:p-0">
+        <h1 className="text-4xl pb-10 font-bold lg:pr-20 ">Hola!! Soy <span className="text-cyan-800 ">Percy Valderrama</span>, Bienvenidos a mi Portafolio</h1>
+        <p className="text-xl lg:pr-20 ">La creatividad es la chispa que da vida a mi trabajo👩‍💻. Cada proyecto es un lienzo en blanco donde puedo dar vida a mis ideas y expresar mi pasión por el codigo. <br /> <br /> Bienvenido a mi mundo creativo 🎨, donde cada detalle cuenta y cada pixel tiene un propósito. Explora mi portafolio y descubre cómo puedo ayudarte a dar vida a tus proyectos.</p>
       </div>
     </div>
   );
