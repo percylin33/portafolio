c42bc55fff7a67b170a46c1e5e90c281f51dd7a0
percy <percy.u.lomas@gmail.com>
2023-10-14 13:24:45 -0500

    style-navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import logo from "@/acces/logo-personal.png"
 import Image from "next/image";
-import foto from "@/acces/Diseño sin título.png"
+
 
 const Navbar = () => {
   return (
@@ -14,7 +14,7 @@ const Navbar = () => {
       </div>
       <div className="sm:ml-20 sm:block">
         <input type="checkbox" id="menu" className="peer hidden" />
-        <label htmlFor="menu" className="bg-open-menu  bg-cover bg-center peer-checked:bg-close-menu transition-all h-8 w-8 absolute top-3 right-4 z-50 md:hidden"><span className="text-cyan-950 z-50">-----</span></label>
+        <label htmlFor="menu" className="bg-open-menu  bg-cover bg-center peer-checked:bg-close-menu transition-all h-8 w-8 absolute top-3 right-4 z-50 md:hidden"></label>
        
         <div className="space-x-8 pt-5 fixed inset-0 bg-gradient-to-b from-cyan-500/70 to-cyan-900/70 translate-x-full peer-checked:translate-x-0 transition-transform z-10 md:static md:flex md:bg-none md:translate-x-0">
        
@@ -41,44 +41,6 @@ const Navbar = () => {
   </div>
 </div>
 
-      {/* Mobile menu */}
-      {/* <div className="sm:hidden" id="mobile-menu">
-        <input type="checkbox" id="menu" />
-        <label htmlFor="menu">Activa/Desactiva</label>
-        <div className="space-y-1 px-2 pb-3 pt-2 ">
-          <Link
-            href="/About"
-            className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
-            aria-current="page"
-          >
-            About
-          </Link>
-          <Link
-            href="/Skills"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-          >
-            Skills
-          </Link>
-          <Link
-            href="/Proyect"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-          >
-            Projects
-          </Link>
-          <Link
-            href="/Experience"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-          >
-            Experience
-          </Link>
-          <Link
-            href="/Contact"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-          >
-            Contact
-          </Link>
-        </div>
-      </div> */}
     </nav>
   );
 };
