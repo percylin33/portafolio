c05546c0a0e964140c457b1a3a45b256f42f6617
percy <percy.u.lomas@gmail.com>
2023-11-02 03:36:37 -0500

    styles-navF3

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -24,8 +24,8 @@ const Navbar = () => {
   return (
     <nav className="bg-custom w-screen relative">
       <div className={`bg-custom shadow-md w-full top-0 left-0 relative z-40`}>
-        <div className="md:flex  items-center justify-around  relative z-40">
-          <div className="flex  items-center">
+        <div className="md:flex  items-center justify-between  relative z-40 ">
+          <div className="flex  items-center pl-10">
             <Link href="/" rel="preload">
               <Image className="h-20 w-auto" src={logo} alt="Your Company" />
             </Link>
@@ -34,7 +34,7 @@ const Navbar = () => {
             {open ? <IoCloseCircleSharp /> : <IoMenu />}
           </div>
           
-      <ul className={`bg-custom md:flex md:items-center md:pb-0 pb-12 md:pr-10 absolute md:static left-0 w-screen md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'left-0 opacity-100 z-20' : 'left-screen md:opacity-100 opacity-0 z-10'}`}>
+      <ul className={`bg-custom md:flex md:items-center  md:pb-0 pb-12 md:pr-10 absolute md:static left-0 w-screen md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'left-0 opacity-100 z-20' : 'left-screen md:opacity-100 opacity-0 z-10'}`}>
         {linkNav.map((li) => (
           <li key={li.name} className="md:ml-8 text-xl md:my-0 my-7" onClick={closeMenu}>
             <Link href={li.link} className="text-white font-medium hover:text-gray-400 duration-500">
