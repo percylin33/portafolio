01dad9de9783a3a2d53eef8e2d0d141d3be6caf3
percy <percy.u.lomas@gmail.com>
2023-11-02 02:14:26 -0500

    styles-finales

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,46 +1,50 @@
-import Link from "next/link";
-import logo from "@/acces/logo-personal.png"
-import Image from "next/image";
+"use client"
 
+import Link from "next/link";
+import logo from "@/acces/logo-personal.png";
+import Image from "next/image";
+import { useState } from 'react';
+import { IoMenu, IoCloseCircleSharp } from 'react-icons/io5';
 
 const Navbar = () => {
+  const linkNav = [
+    { name: "About", link: "/About" },
+    { name: "Skills", link: "/Skills" },
+    { name: "Projects", link: "/Proyect" },
+    { name: "Experience", link: "/Experience" },
+    { name: "Contact", link: "/Contact" },
+  ];
+
+  const [open, setOpen] = useState(false);
+
+  const closeMenu = () => {
+    setOpen(false);
+  };
+
   return (
-    <nav className="bg-custom w-screen  ">
-     <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-  <div className="relative flex h-16 items-center justify-between">
-    <div className="flex flex-1 items-center sm:items-stretch justify-start">
-      <div className="flex flex-shrink-0 items-center">
-        <Link href="/"><Image className="h-20 w-auto" src={logo} alt="Your Company" /></Link>
-      </div>
-      <div className="sm:ml-20 sm:block">
-        <input type="checkbox" id="menu" className="peer hidden" />
-        <label htmlFor="menu" className="bg-open-menu  bg-cover bg-center peer-checked:bg-close-menu transition-all h-8 w-8 absolute top-3 right-4 z-50 md:hidden"></label>
-       
-        <div className="space-x-8 pt-5 fixed inset-0 bg-gradient-to-b from-cyan-500/70 to-cyan-900/70 translate-x-full peer-checked:translate-x-0 transition-transform z-10 md:static md:flex md:bg-none md:translate-x-0">
-       
-          <div className="absolute inset-0 top-20 p-12 bg-slate-300 w-[80%] mx-auto h-max rounded-md text-center grid gap-6 font-bold shadow-2xl md:w-max md:bg-transparent md:p-0 md:grid-flow-col md:static ">
-            <Link href="/About" className=" hover:bg-hov text-black rounded-md px-3 py-2 text-sm font-medium md:text-white" >
-              <p className="font-bold">About</p>
-            </Link>
-            <Link href="/Skills" className=" hover:bg-hov text-black rounded-md px-3 py-2 text-sm font-medium md:text-white">
-              <p className="font-bold">Skills</p>
-            </Link>
-            <Link href="/Proyect" className=" hover:bg-hov text-black rounded-md px-3 py-2 text-sm font-medium md:text-white">
-              <p className="font-bold">Projects</p>
-            </Link>
-            <Link href="/Experience" className=" hover:bg-hov text-black rounded-md px-3 py-2 text-sm font-medium md:text-white">
-              <p className="font-bold">Experience</p>
-            </Link>
-            <Link href="/Contact" className=" hover:bg-hov text-black rounded-md px-3 py-2 text-sm font-medium md:text-white">
-              <p className="font-bold">Contact</p>
+    <nav className="bg-custom w-screen relative">
+      <div className={`bg-custom shadow-md w-full top-0 left-0 relative z-40`}>
+        <div className="md:flex  items-center justify-around  relative z-40">
+          <div className="flex  items-center">
+            <Link href="/" rel="preload">
+              <Image className="h-20 w-auto" src={logo} alt="Your Company" />
             </Link>
           </div>
+          <div onClick={() => setOpen(!open)} className="text-white text-4xl absolute right-8 top-4 cursor-pointer md:hidden">
+            {open ? <IoCloseCircleSharp /> : <IoMenu />}
+          </div>
+          
+      <ul className={`bg-custom md:flex md:items-center md:pb-0 pb-12 md:pr-10 absolute md:static left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'left-0 opacity-100 z-20' : 'left-full md:opacity-100 opacity-0 z-10'}`}>
+        {linkNav.map((li) => (
+          <li key={li.name} className="md:ml-8 text-xl md:my-0 my-7" onClick={closeMenu}>
+            <Link href={li.link} className="text-white font-medium hover:text-gray-400 duration-500">
+              {li.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
         </div>
       </div>
-    </div>
-  </div>
-</div>
-
     </nav>
   );
 };
