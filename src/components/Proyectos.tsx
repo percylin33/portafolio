b60f39bdd972c6e103426b061fb3529db9f0abc5
percy <percy.u.lomas@gmail.com>
2023-11-16 18:51:52 -0500

    cuso-next

--- src/components/Proyectos.tsx
+++ src/components/Proyectos.tsx
@@ -13,7 +13,7 @@ const Proyectos = () => {
             <div  className="grow relative bg-cover bg-center opacity-80 mx-6 rounded-md" style={{ backgroundImage: 'url("/imagenes/fondo.jpg")' }}>    
             </div>
             <div className="p-2 px-6 max-h-[40%]  overflow-y-auto  scrollbar-thumb-slate-400 scrollbar-thin scrollbar-track-slate-300 ">
-              <p className="font-medium ">Proyecto donde aplico mis habilidades en React, CSS, Redux, Express y Postgresql.</p>
+              <p className="font-medium ">Proyecto realizado durante el bootcamp donde aplico mis habilidades en React, CSS, Redux, Express y Postgresql.</p>
             </div>
 
             <div className="flex justify-center bottom-0 pb-2 items-center  ">
@@ -31,7 +31,7 @@ const Proyectos = () => {
             <div  className="grow h-[40%] relative bg-cover bg-center opacity-80 mx-6 rounded-md" style={{ backgroundImage: 'url("/imagenes/pokemon.png")' }}>    
             </div>
             <div className="p-2 px-6 max-h-[40%]  overflow-y-auto  scrollbar-thumb-slate-400 scrollbar-thin scrollbar-track-slate-300 ">
-              <p className="font-medium ">App donde implementé diversos filtros y operaciones CRUD aplicando con JS, React, Redux, Express, Node.js, Postgresql.</p>
+              <p className="font-medium ">App donde implementé diversos filtros y operaciones CRUD aplicando con JS, React, Redux, Express, NodeJS, Postgresql.</p>
             </div>
 
             <div className="flex justify-center bottom-0 pb-2 items-center ">
@@ -49,7 +49,7 @@ const Proyectos = () => {
             <div  className="grow relative bg-cover bg-center opacity-80 mx-6 rounded-md" style={{ backgroundImage: 'url("/imagenes/logoIcon.png")' }}>    
             </div>
             <div className="p-2 px-6  max-h-[40%]  overflow-y-auto  scrollbar-thumb-slate-400 scrollbar-thin scrollbar-track-slate-300  ">
-              <p className="font-medium ">Proyecto grupal ecommerce inmobiliario con Pasarelas de pago · React.js, Node.js, MongoDB, Next.js, Tailwind-CSS y TypeScript.</p>
+              <p className="font-medium ">Proyecto grupal ecommerce inmobiliario con Pasarelas de pago · React, NodeJS, MongoDB, Mercado Pago, Cloudinary, NextJS, Tailwind-CSS y TypeScript.</p>
             </div>
 
             <div className="flex justify-center bottom-0 pb-2 items-center  ">
@@ -66,8 +66,8 @@ const Proyectos = () => {
             <p className="text-center text-xl mx-auto py-2 font-bold ">Portafolio</p>
             <div  className="grow relative bg-cover bg-center opacity-80 mx-6 rounded-md" style={{ backgroundImage: 'url("/imagenes/logo-personal.png")' }}>    
             </div>
-            <div className="p-2 px-6 max-h-[40%]  overflow-y-auto  scrollbar-thumb-cyan-400 scrollbar-thin scrollbar-track-slate-300 ">
-              <p className="font-medium ">Proyecto donde utilizo mis habilidades en TypeScript, Next, Tailwind-CSS y React</p>
+            <div className="p-2 px-6 max-h-[40%]  overflow-y-auto  scrollbar-thumb-slate-400 scrollbar-thin scrollbar-track-slate-300  ">
+              <p className="font-medium ">Proyecto en el que creo un portafolio personalizado donde utilizo mis habilidades en TypeScript, Next, Tailwind-CSS y React</p>
             </div>
 
             <div className="flex justify-center bottom-0 pb-2 items-center  ">
@@ -81,18 +81,18 @@ const Proyectos = () => {
         </div>
 
         <div className="bg-cyan-50 h-64 flex flex-col rounded-md shadow-xl hover:bg-cyan-100 hover:shadow-2xl hover:scale-105 transition duration-300 ">
-            <p className="text-center text-xl mx-auto py-2 font-bold ">Portafolio</p>
-            <div  className="grow relative bg-cover bg-center opacity-80 mx-6 rounded-md" style={{ backgroundImage: 'url("/imagenes/logo-personal.png")' }}>    
+            <p className="text-center text-xl mx-auto py-2 font-bold ">Dashboard</p>
+            <div  className="grow relative bg-cover bg-center opacity-80 mx-6 rounded-md" style={{ backgroundImage: 'url("/imagenes/curso-next.png")' }}>    
             </div>
-            <div className="p-2 px-6 max-h-[40%]  overflow-y-auto  scrollbar-thumb-cyan-400 scrollbar-thin scrollbar-track-slate-300 ">
-              <p className="font-medium ">Proyecto donde utilizo mis habilidades en TypeScript, Next, Tailwind-CSS y React</p>
+            <div className="p-2 px-6 max-h-[40%]  overflow-y-auto  scrollbar-thumb-slate-400 scrollbar-thin scrollbar-track-slate-300 ">
+              <p className="font-medium ">Proyecto de un Dashboard donde implemento lo nuevo de NextJS 14, NextAuth, TypeScript, Tailwind-CSS y React</p>
             </div>
 
             <div className="flex justify-center bottom-0 pb-2 items-center  ">
-                <a href="https://github.com/percylin33/portafolio" target="_blank" rel="noopener noreferrer " className=" w-6 mx-auto hover:scale-110 transition duration-300 ">
+                <a href="https://github.com/percylin33/cuso-next" target="_blank" rel="noopener noreferrer " className=" w-6 mx-auto hover:scale-110 transition duration-300 ">
                   <Image src={github} alt="link" className="w-auto"/>
                 </a>
-                <a href="https://portafolio-pied-six.vercel.app/" target="_blank" rel="noopener noreferrer " className=" w-6 mx-auto hover:scale-110 transition duration-300" >
+                <a href="https://cuso-next.vercel.app/" target="_blank" rel="noopener noreferrer " className=" w-6 mx-auto hover:scale-110 transition duration-300" >
                   <Image src={link} alt="link" className="w-full"/>
                 </a>
             </div>
