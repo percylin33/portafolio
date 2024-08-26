a4bfef2249b83fb4a8200733e8be6e1f7b6dffef
percy <p3r.valderrama@gmail.com>
2024-08-26 10:40:37 -0500

    notion-add

--- src/app/Proyect/page.tsx
+++ src/app/Proyect/page.tsx
@@ -1,6 +1,7 @@
 
 import ImageCarousel from "@/components/Carrousel";
 import Proyectos from "@/components/Proyectos";
+import { IoLogoHtml5 } from "react-icons/io5";
 
 const Proyect = () => {
     return <div className="bg-gray-100 h-full w-screen md:h-screen lg:h-[calc(100vh-4rem)] "> 
